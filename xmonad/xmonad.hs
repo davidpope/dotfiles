6b074234c7458ebf0b5f46a9ac20ffd76fb469c3
Dave Pope <davep@davep-linux-vm.(none)>
2013-02-06 00:24:57 -0600

    xmonad tweaks

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,24 +6,38 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import qualified XMonad.StackSet as W
 
+import XMonad.Layout.Reflect
+
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/davep/.xmobarrc"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        -- , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , layoutHook = myLayout
         , logHook = dynamicLogWithPP xmobarPP
                     { ppOutput = hPutStrLn xmproc
                     , ppTitle = xmobarColor "green" "" . shorten 50
                     }
         , modMask = mod4Mask    -- Rebind Mod to the Windows key
-        , borderWidth = 2
+        , borderWidth = 5
         } `additionalKeys` myKeys
 
 myKeys =
     [
     ]
+    -- adjust physical display ordering in xmobar
     ++
     [((m .|. mod4Mask, key), screenWorkspace sc >>= flip whenJust (windows . f)) -- Replace 'mod1Mask' with your mod key of choice.
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [1,0,2] -- was [0..] *** change to match your screen order ***
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
     ]
+
+myLayout = avoidStruts
+           $ tiled ||| wideTiled ||| reflectHoriz tiled ||| reflectHoriz wideTiled ||| Full
+            where
+                tiled = Tall nmaster delta ratio
+                wideTiled = Tall nmaster delta wideRatio
+                nmaster = 1
+                delta = 3/100
+                ratio = 1/2
+                wideRatio = 2/3
