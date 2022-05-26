1d5a9bcf0dc760a6e1d9c20c383b08d011aa19b1
David Pope <david@davidpope.org>
2022-05-26 18:11:04 -0500

    remove xmonad setup

--- setup-links.sh
+++ setup-links.sh
@@ -45,25 +45,6 @@ if [ "x$1" == "xi3" ]; then
     $SCRIPT_DIR/i3wm/setup.sh
 elif [ "x$1" == "xgnome3" ]; then
     $SCRIPT_DIR/gnome3/setup-workspaces.sh
-elif [ "x$1" == "xxmonad" ]; then
-    if [ -d $HOME/.xmonad ]; then
-        echo "$HOME/.xmonad directory already exists, aborting XMonad setup."
-    else
-        mkdir $HOME/.xmonad
-        [ -a $HOME/.xmonad/xmonad.hs ] || ln -s $SCRIPT_DIR/xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs
-        [ -a $HOME/.Xresources ] || ln -s $SCRIPT_DIR/xmonad/Xresources $HOME/.Xresources
-        [ -a $HOME/.xmobarrc ] || ln -s $SCRIPT_DIR/xmonad/xmobarrc $HOME/.xmobarrc
-        [ -a $HOME/.xinitrc ] || ln -s $SCRIPT_DIR/xmonad/xinitrc $HOME/.xinitrc
-
-        echo "If a *dm entry for a user-defined session does not exist, run the following:"
-        echo "    sudo cp $SCRIPT_DIR/custom.desktop /usr/share/xsessions"
-
-        # for *dm-managed login
-        [ -a $HOME/.xsession ] || ln -s $HOME/.xinitrc $HOME/.xsession
-
-        echo "XMonad setup complete."
-    fi
-
 else
-    echo "Skipping window manager setup, use '$0 i3', '$0 gnome3', '$0 awesome', or '$0 xmonad' if that is desired."
+    echo "Skipping window manager setup, use '$0 i3' or '$0 gnome3' if that is desired."
 fi
